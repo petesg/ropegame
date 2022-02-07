14991129a6aea86b078e2c423a3eb8c3dac0cb72
petesg <psgmechtech@gmail.com>
2022-02-07 17:54:31 -0600

    some nonworking stubs for collision detection

--- collision.c
+++ collision.c
@@ -10,7 +10,7 @@ void initCollision(void) {
     addCollider(125, 150, 200, 225);
 }
 
-Collider* addCollider(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2) {
+Collider* addCollider(uint32_t x1, uint32_t y1, uint32_t x2, uint32_t y2) {
     printf("adding collider to colliders[%d]\n", numColliders);
     Collider* tempPtr = realloc(colliders, sizeof(Collider) * (numColliders + 1));
     if (!tempPtr) {
@@ -38,7 +38,22 @@ uint16_t getNearbyColliders(uint16_t pos[2], uint8_t radius, Collider** dest) {
 // moves the actor along its velocity vector
 // if a collision is detected, pointer to interfering collider will be returned
 Collider* moveActor(Actor* a) {
+
+    double vMag = sqrt(pow(a->v[0], 2) + pow(a->v[1], 2));  // magnitude of v
+    uint16_t numSteps = vMag / (2 * a->hitboxRad);          // number of preliminary steps to check 
+    double norm[] = {a->v[0] / vMag, a->v[1] / vMag};       // normalized vector along direction of v
+    uint32_t goodPos[] = {a->pos[0], a->pos[1]};              // furthest forward position along v known to be safe (updated as we go along)
+
+    for (uint16_t i = 0; i < numSteps; ++i) {
+
+    }
+    
     a->pos[0] += a->v[0];
     a->pos[1] += a->v[1];
+
     return NULL;
+}
+
+bool testCollision(uint32_t* p1, uint32_t* p2, uint32_t* p3, uint16_t r) {
+    
 }
--- collision.h
+++ collision.h
@@ -2,6 +2,7 @@
 #define COLLISION_H
 
 #include <stdio.h>
+#include <math.h>
 
 #include "structs.h"
 #include "errors.h"
@@ -10,8 +11,9 @@ extern Collider* colliders;
 extern uint32_t numColliders;
 
 void initCollision(void);
-Collider* addCollider(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2);
+Collider* addCollider(uint32_t x1, uint32_t y1, uint32_t x2, uint32_t y2);
 uint16_t getNearbyColliders(uint16_t pos[2], uint8_t radius, Collider** dest);
 Collider* moveActor(Actor* a);
+bool testCollision(uint32_t* p1, uint32_t* p2, uint32_t* p3, uint16_t r);
 
 #endif // COLLISION_H
